1b3f79937d1944c3aab7d1a2c28d6b287cee4dd9
ekta12345 <ektakumbhare3@gmail.com>
2021-04-29 15:58:42 +0530

    Added:To Inform the user if invalid mood is entered

---
+++ MoodAnalyserPrb/MoodAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserPrb
+{
+    public class MoodAnalyserException:Exception
+    {
+        ExceptionType type;  //Create instance of custom exception type
+        public string message;
+
+        public enum ExceptionType  //enum for define constants
+        {
+            NULL_EXCEPTION, EMPTY_EXCEPTION
+        }
+        public MoodAnalyserException(ExceptionType type, string message) : base(message)  //parameterized constructor and also using bsae method
+        {
+            this.type = type;
+        }
+    }
+}
--- MoodAnalyserPrb/MoodAnalyzer.cs
+++ MoodAnalyserPrb/MoodAnalyzer.cs
@@ -8,31 +8,44 @@ namespace MoodAnalyserPrb
 {
     public class MoodAnalyzer
     {
-        public string message;  //instance variable      
+        public string message;  //instance variable
 
 
+        public MoodAnalyzer() //Constructors
+        {
+
+        }
         public MoodAnalyzer(string message) //parameterized constructor for intilizing instance member
         {
             this.message = message;
+            //this.methodName = methodName;
         }
         public string Analyzer()  //Analyzer method find mood
         {
             try  // Handling Exception
             {
-                if (this.message.ToLower().Contains("happy"))
+                if (this.message.Equals(string.Empty))
                 {
-                    return "happy";
+
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
                 }
                 else
                 {
-                    return "sad";
+                    if (this.message.ToLower().Contains("happy"))
+                    {
+                        return "happy";
+                    }
+                    else
+                    {
+                        return "sad";
+                    }
                 }
             }
-            catch
-            //UC2 use --> //catch(NullReferenceException ex)
+            catch (NullReferenceException ex)
             {
                 //UC2 use -->// return ex.Message;
-                return "happy";
+                // return "happy";
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL_EXCEPTION, "Mood should not be null");
             }
         }
     }
