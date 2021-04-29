95c1be22ebae69f8c002c2d448ba61c6c66473c0
ekta-12345 <ektakumbhare3@gmail.com>
2021-04-29 16:24:35 +0530

    Update UnitTest1.cs

--- MSTestMoodAnalyzer/UnitTest1.cs
+++ MSTestMoodAnalyzer/UnitTest1.cs
@@ -63,73 +63,7 @@ namespace MSTestMoodAnalyzer
 
 
         }
-        using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using MoodAnalyserPrb;
-
-
-namespace MSTestMoodAnalyzer
-{
-    
-    [TestClass]
-    public class UnitTest1  //Class
-    {
-        /* TC 1.1:- Given “I am in Sad Mood” message Should Return SAD
-                    - analyseMood method can just return SAD to pass this Test Case (TC)
-        */
-        [TestMethod]
-        public void AnalyseMood_ShouldReturn_Sad()
-        {
-            //Arraneg
-            string expected = "sad";
-            MoodAnalyzer obj = new MoodAnalyzer("I am in sad Mood");
-            //Act
-            string actual = obj.Analyzer();
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        /* TC 1.2:- Given “I am in Any Mood” message Should Return HAPPY.
-                    - To make the Test case pass analyseMood method need to check for Sad else return HAPPY
-       */
-        [TestMethod]
-        public void Given_Happymood_Expecting_Happy_Result()  //Method
-        {
-            MoodAnalyzer mood = new MoodAnalyzer("I am in happy mood"); //Create object and arrange 
-            string expected = "happy";
-
-            string actual = mood.Analyzer();    //act
-            Assert.AreEqual(expected, actual);  //Assert
-        }
-
-        /* UC2:- Handle Exception if User Provides Invalid Mood
-         */
-        [TestMethod]
-        public void Given_Nullmood_Expecting_Exception_Result()  //Method
-        {
-            MoodAnalyzer mood = new MoodAnalyzer(null); //Create object and arrange 
-            string expected = "Object reference not set to an instance of an object.";
-
-            string actual = mood.Analyzer();    //act
-
-            Assert.AreEqual(expected, actual);  //Assert
-        }
-
-        /* TC 2.1:- Given Null Mood Should Return Happy.
-                   - To make this Test Case pass Handle NULL Scenario using try catch and return Happy.
-        */
-        [TestMethod]
-        public void Given_Nullmood_Expecting_happy_Result()  //Method
-        {
-            MoodAnalyzer mood = new MoodAnalyzer(null); //Create object and arrange 
-            string expected = "happy";
-
-            string actual = mood.Analyzer();    //act
-
-            Assert.AreEqual(expected, actual);  //Assert
-        }
-
+        
         /* TC 3.1:- NULL Given NULL Mood Should Throw MoodAnalysisException
                     To pass this Test Case in try catch block throw MoodAnalysisException
          */
@@ -169,7 +103,8 @@ namespace MSTestMoodAnalyzer
             {
                 Assert.AreEqual("Mood should not be empty", exception.Message);  //Assert
             }
+         }
+      }
+   }
 
-        }
-    }
-}
+        
